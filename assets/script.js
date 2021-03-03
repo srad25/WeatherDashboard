5f1a35e2fc49f0d6f70a168a40bb552b6dc7dd95
srad25 <suzanne.radlein@gmail.com>
2021-03-03 15:09:30 -0500

    function to get city weather

--- assets/script.js
+++ assets/script.js
@@ -53,3 +53,51 @@ var getCityData = function(event){
 }
 searchForm.addEventListener("submit", getCity);
 loggedBtnEl.addEventListener("click", getCityData);
+
+var getCityWeather = function(city){
+    apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+   fetch(apiURL)
+   .then(function(response){
+       response.json().then(function(data){
+           displayWeather(data, city);
+       });
+   });
+};
+
+var displayWeather = function(weather, searchCity){
+  //Clear old content
+  currentWeatherEl.textContent= "";  
+  cityNameEl.textContent=searchCity;
+
+  //Date element
+  var currentDate = document.createElement("span")
+  currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+  cityNameEl.appendChild(currentDate);
+
+  //Image element
+  var weatherIcon = document.createElement("img")
+  weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+  cityNameEl.appendChild(weatherIcon);
+
+  //create a span for temp, humidity, wind
+  var temperatureEl = document.createElement("span");
+  temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+  temperatureEl.classList = "list-group-item"
+ 
+  
+  var humidityEl = document.createElement("span");
+  humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+  humidityEl.classList = "list-group-item"
+
+  
+  var windSpeedEl = document.createElement("span");
+  windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+  windSpeedEl.classList = "list-group-item"
+
+  //append to container
+  currentWeatherEl.appendChild(temperatureEl);
+
+  currentWeatherEl.appendChild(humidityEl);
+
+  currentWeatherEl.appendChild(windSpeedEl);
